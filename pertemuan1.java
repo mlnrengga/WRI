bf716e90b6f67a5ce9f3c968d8610c68a2eeaa74
mlnrengga <mlnrengga@gmail.com>
2023-11-01 20:43:44 +0700

    tambah teman

--- pertemuan1.java
+++ pertemuan1.java
@@ -2,5 +2,6 @@ public class pertemuan1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Halo dunia");
+        System.out.println("Halo polinema");
     }
 }
